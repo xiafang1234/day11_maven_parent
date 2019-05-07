d8aded4c3ac8a57b454a78b6293a265c2b2f33ed
admin <2022627889@qq.com>
2019-05-07 18:16:04 +0800

    Initial commit

---
+++ day11_maven_service/src/main/java/cn/itcast/service/ItemsService.java
@@ -0,0 +1,10 @@
+package cn.itcast.service;
+
+import cn.itcast.domain.Items;
+
+
+public interface ItemsService {
+
+    public Items findById(Integer id);
+
+}
---
+++ day11_maven_service/src/main/java/cn/itcast/service/impl/ItemsServiceImpl.java
@@ -0,0 +1,18 @@
+package cn.itcast.service.impl;
+
+import cn.itcast.dao.ItemsDao;
+import cn.itcast.domain.Items;
+import cn.itcast.service.ItemsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ItemsServiceImpl implements ItemsService {
+
+    @Autowired
+    private ItemsDao itemsDao;
+
+    public Items findById(Integer id) {
+        return itemsDao.findById(id);
+    }
+}
